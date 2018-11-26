7c80968d7be0d7cb5e44900573408491a867e316
julio16j <voce@exemplo.com>
2018-11-26 04:59:06 -0300

    Merge branch 'master' of https://github.com/julio16j/Teste
    # Conflicts:
    #	polls/views.py

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import  Question
+from django.template import loader, RequestContext
 # Create your views here.
 def index(request):
+    latest_questions = Question.objects.order_by('pub_date')[:5]
+    context = {'latest_questions': latest_questions}
+    return render (request, 'polls/index.html',context)
 
-    return HttpResponse('UAU HAHAHAHA')
+def detail(response,question_id):
+    return HttpResponse('detail view of the question: %s' %question_id)
 
+def results(response ,question_id):
+    return HttpResponse("results of the question : %s" %question_id)
+def vote( response,question_id):
+    return HttpResponse("vote on question: %s" %question_id)
