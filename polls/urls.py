9708a29358cdaaadbad4862bc8b6bbc7491a8b59
julio16j <voce@exemplo.com>
2018-11-26 16:15:56 -0300

    Merge branch 'master' of https://github.com/julio16j/Teste
    # Conflicts:
    #	polls/views.py

--- polls/urls.py
+++ polls/urls.py
@@ -6,10 +6,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name="index"),
     #127.0.0.1/polls
-    path('/<question_id>/', views.detail, name="detail"),
-    #127.0.0.1/polls/1
-    path('/results/<question_id>/', views.results, name="result"),
-    # 127.0.0.1/polls/1/results
-    path('/vote/<question_id>/', views.vote, name="vote"),
-    #127.0.0.1/polls/1/vote
+    path('/envia/<lat>/<long>/', views.envia, name="envia"),
+    # 127.0.0.1/polls/1
 ]
